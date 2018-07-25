a229abd2b0e92ad93187a63338df8883be5db647
dezhizhang <1541609448@qq,com>
2018-07-25 22:37:14 +0800

    热销推荐布局

--- src/pages/home/Home.vue
+++ src/pages/home/Home.vue
@@ -3,22 +3,23 @@
       <Header/>
       <HomeSwiper/>
       <HomeIcons/>
+      <Recommend/>
    </div>
 </template>
 
 <script>
-import Header from './components/Header'
-import HomeSwiper from './components/HomeSwiper'
-import HomeIcons from './components/HomeIcons'
+import Header from './components/Header';
+import HomeSwiper from './components/HomeSwiper';
+import HomeIcons from './components/HomeIcons';
+import Recommend from './components/Recommend';
 export default{
     name:'Home',
     components:{
       Header,
       HomeSwiper,
       HomeIcons,
+      Recommend,
       
-
-
     }
 }
 </script>
---
+++ src/pages/home/components/Recommend.vue
@@ -0,0 +1,125 @@
+<template>
+ <div class='recommend'>
+   <div class='recommend-title'>推销推荐</div>
+   <ul>
+     <li class='item' v-for='item in recommendList' :key='item.id'>
+          <img class='item-img' :src='item.imgUrl'/>
+        <div>
+          <p class='item-title'>{{item.title}}</p>
+          <p class='item-desc'>{{item.desc}}</p>
+          <button class='item-btn'>查看详情</button>
+        </div>
+     </li>
+   </ul>
+ </div>
+</template>
+
+<script>
+import uuid from 'uuid';
+export default{
+    name:'Recommend',
+    data(){
+        return {
+            recommendList:[
+                {
+                  id:uuid(),
+                  imgUrl:'https://ss2.bdstatic.com/70cFvnSh_Q1YnxGkpoWK1HF6hhy/it/u=1830359176,654163576&fm=200&gp=0.jpg',
+                  title:'大连圣亚海洋世界',
+                  desc:'浪漫大连首都，浪漫主题海洋乐园'
+                },
+                {
+                  id:uuid(),
+                  imgUrl:'https://ss1.bdstatic.com/70cFuXSh_Q1YnxGkpoWK1HF6hhy/it/u=3033037990,1425076943&fm=200&gp=0.jpg',
+                  title:'大连圣亚海洋世界',
+                  desc:'浪漫大连首都，浪漫主题海洋乐园'
+                },
+                {
+                  id:uuid(),
+                  imgUrl:'https://ss0.bdstatic.com/70cFuHSh_Q1YnxGkpoWK1HF6hhy/it/u=3545575964,2149126446&fm=27&gp=0.jpg',
+                  title:'大连圣亚海洋世界',
+                  desc:'浪漫大连首都，浪漫主题海洋乐园'
+                },
+                {
+                  id:uuid(),
+                  imgUrl:'https://ss0.bdstatic.com/70cFuHSh_Q1YnxGkpoWK1HF6hhy/it/u=2944094007,2505799703&fm=27&gp=0.jpg',
+                  title:'大连圣亚海洋世界',
+                  desc:'浪漫大连首都，浪漫主题海洋乐园'
+                },
+                {
+                  id:uuid(),
+                  imgUrl:'https://ss2.bdstatic.com/70cFvnSh_Q1YnxGkpoWK1HF6hhy/it/u=1830359176,654163576&fm=200&gp=0.jpg',
+                  title:'大连圣亚海洋世界',
+                  desc:'浪漫大连首都，浪漫主题海洋乐园'
+                },
+                {
+                  id:uuid(),
+                  imgUrl:'https://ss1.bdstatic.com/70cFuXSh_Q1YnxGkpoWK1HF6hhy/it/u=3033037990,1425076943&fm=200&gp=0.jpg',
+                  title:'大连圣亚海洋世界',
+                  desc:'浪漫大连首都，浪漫主题海洋乐园'
+                },
+                {
+                  id:uuid(),
+                  imgUrl:'https://ss0.bdstatic.com/70cFuHSh_Q1YnxGkpoWK1HF6hhy/it/u=3545575964,2149126446&fm=27&gp=0.jpg',
+                  title:'大连圣亚海洋世界',
+                  desc:'浪漫大连首都，浪漫主题海洋乐园'
+                },
+                {
+                  id:uuid(),
+                  imgUrl:'https://ss0.bdstatic.com/70cFuHSh_Q1YnxGkpoWK1HF6hhy/it/u=2944094007,2505799703&fm=27&gp=0.jpg',
+                  title:'大连圣亚海洋世界',
+                  desc:'浪漫大连首都，浪漫主题海洋乐园'
+                }
+            ]
+        }
+    }
+
+}
+</script>
+
+<style scoped>
+.recommend-title{
+    margin-top:.1rem;
+    line-height:.8rem;
+    background:#eee;
+    text-indent:.2rem;
+}
+.item{
+    overflow:hidden;
+    display:flex;
+    height:1.9rem;
+    border-bottom:.01rem solid #ccc;
+}
+.item-img{
+    width:1.7rem;
+    height:1.7rem;
+    padding:.1rem;
+
+}
+.item-info{
+    flex:1;
+    padding:.1rem;
+    min-width:0;
+
+}
+.item-title{
+    line-height:.54rem;
+    font-size:.32rem;
+
+}
+.item-desc{
+    line-height:.4rem;
+    color:#ccc;
+
+}
+.item-btn{
+    margin-top:.2rem;
+    line-height:.24rem;
+    background:#ff9300;
+    padding:.1rem;
+    border-radius:.06rem;
+    margin-left:.2rem;
+    color:#fff;
+    font-size:.2rem;
+
+}
+</style>
